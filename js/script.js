2201553c18b0888cb5dd7d41f8d2634219004371
h1n3s1ght <benjaminisaachines@gmail.com>
2022-05-10 14:09:23 -0500

    functioning JS calls

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // alert("working");
 
 //Variables----------------------------------------------
-const URL = "https://dark-sky.p.rapidapi.com/32.7157,-117.1611?units=auto&extend=168&lang=en"
+let URL = "https://dark-sky.p.rapidapi.com/32.7157,-117.1611?units=auto&extend=168&lang=en"
 
 
 //Element References----------------------------------------------
@@ -11,8 +11,9 @@ let $startMonth = $('.start_month');
 let $endMonth = $('.end_month');
 let $startDay = $('.start_month');
 let $endDay = $('.end_month');
-let $startDate = '';
-let $endDate = '';
+let $startDate = [];
+let $endDate = [];
+let $numOfHours = '';
 
 
 //Event Listeners----------------------------------------------
@@ -20,6 +21,23 @@ let $endDate = '';
 
 
 //Functions----------------------------------------------
+
+//Create a function to call the Ajax URL
+function runApi(){
+    if($location === 'sanDiego'){
+        URL = `https://dark-sky.p.rapidapi.com/32.7157,-117.1611?units=auto&extend=${$numOfHours}&lang=en`;
+    } else if ($location === 'yellowStone'){
+        URL = `https://dark-sky.p.rapidapi.com/44.4280,-110.5885?units=auto&extend=${$numOfHours}&lang=en`;
+    } else if ($location === 'newYork'){
+        URL = `https://dark-sky.p.rapidapi.com/40.7128,-74.0060?units=auto&extend=${$numOfHours}&lang=en`;
+    } else if ($location === 'miami'){
+        URL = `https://dark-sky.p.rapidapi.com/25.7617,-80.1918?units=auto&extend=${$numOfHours}&lang=en`;
+    } else if ($location === 'zion'){
+        URL = `https://dark-sky.p.rapidapi.com/37.2982,-113.0263?units=auto&extend=${$numOfHours}&lang=en`;
+    }  else {
+        console.log('location does not match listed options.')
+    }
+}
 
 //Write function to change the URL variable when a different selection is made
 $('button').on('click', function(){
@@ -29,5 +47,10 @@ $startMonth = $('select.start_month option:selected').val();
 $startDay = $('select.start_day option:selected').val();
 $endMonth = $('select.end_month option:selected').val();
 $endDay = $('select.end_day option:selected').val();
-console.log($location, $activities , $endDay, $endMonth, $startDay , $startMonth);
+$startDate = (`${$startMonth} ${$startDay}`);
+$endDate = (`${$endMonth} ${$endDay}`);
+$numOfHours = (($endDay - $startDay)* 24);
+console.log($location, $activities , $startDate, $endDate , $numOfHours);
+runApi();
+console.log(URL);
 });
