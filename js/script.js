c38c5819471cfd5592c8eeec1b2eca8fd1ad5ef1
h1n3s1ght <benjaminisaachines@gmail.com>
2022-05-10 11:41:40 -0500

    Get the Submit button functioning with Variables

--- js/script.js
+++ js/script.js
@@ -5,8 +5,14 @@ const URL = "https://dark-sky.p.rapidapi.com/32.7157,-117.1611?units=auto&extend
 
 
 //Element References----------------------------------------------
-let $location = $('#places');
-let $activities = $('typeOfT');
+let $location = $('.places');
+let $activities = $('.typeOfT');
+let $startMonth = $('.start_month');
+let $endMonth = $('.end_month');
+let $startDay = $('.start_month');
+let $endDay = $('.end_month');
+let $startDate = '';
+let $endDate = '';
 
 
 //Event Listeners----------------------------------------------
@@ -17,7 +23,11 @@ let $activities = $('typeOfT');
 
 //Write function to change the URL variable when a different selection is made
 $('button').on('click', function(){
-$activities = $('select.typeOfT option:selected')
+$activities = $('select.typeOfT option:selected').val();
 $location = $('select.places option:selected').val();
-console.log($location);
+$startMonth = $('select.start_month option:selected').val();
+$startDay = $('select.start_day option:selected').val();
+$endMonth = $('select.end_month option:selected').val();
+$endDay = $('select.end_day option:selected').val();
+console.log($location, $activities , $endDay, $endMonth, $startDay , $startMonth);
 });
