54af4cdeea540ac776373fc9b15ed734148d1dff
h1n3s1ght <benjaminisaachines@gmail.com>
2022-05-10 09:57:10 -0500

    Start of JS Coding

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 // alert("working");
+
+//Variables----------------------------------------------
+const URL = "https://dark-sky.p.rapidapi.com/32.7157,-117.1611?units=auto&extend=168&lang=en"
+
+
+//Element References----------------------------------------------
+let $location = $('#places');
+let $activities = $('typeOfT');
+
+
+//Event Listeners----------------------------------------------
+
+
+
+//Functions----------------------------------------------
+
+//Write function to change the URL variable when a different selection is made
+$('button').on('click', function(){
+$activities = $('select.typeOfT option:selected')
+$location = $('select.places option:selected').val();
+console.log($location);
+});
