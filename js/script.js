7dd2a4cd9787ef2e8df837fcb233985e70a2ff71
h1n3s1ght <benjaminisaachines@gmail.com>
2022-05-10 16:21:26 -0500

    JS updated with working/connected API: Dark Sky

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 // alert("working");
 
 //Variables----------------------------------------------
-let URL = "https://dark-sky.p.rapidapi.com/32.7157,-117.1611?units=auto&extend=168&lang=en"
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": " ",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Host": "dark-sky.p.rapidapi.com",
+		"X-RapidAPI-Key": "3282c2cba4msh00975632392a7f6p11275fjsne77488776eae"
+	}
+};
 
-
-//Element References----------------------------------------------
 let $location = $('.places');
 let $activities = $('.typeOfT');
 let $startMonth = $('.start_month');
@@ -15,6 +22,9 @@ let $startDate = [];
 let $endDate = [];
 let $numOfHours = '';
 
+//Element References----------------------------------------------
+
+
 
 //Event Listeners----------------------------------------------
 
@@ -22,25 +32,34 @@ let $numOfHours = '';
 
 //Functions----------------------------------------------
 
-//Create a function to call the Ajax URL
+//Hide the last paragraph at load of page
+$(document).ready(function(){
+    $('#afterSubmit').hide();
+});
+
+//Write function that selects the correct API link
+
 function runApi(){
     if($location === 'sanDiego'){
-        URL = `https://dark-sky.p.rapidapi.com/32.7157,-117.1611?units=auto&extend=${$numOfHours}&lang=en`;
+        settings.url = `https://dark-sky.p.rapidapi.com/32.7157,-117.1611?units=auto&extend=${$numOfHours}&lang=en`;
     } else if ($location === 'yellowStone'){
-        URL = `https://dark-sky.p.rapidapi.com/44.4280,-110.5885?units=auto&extend=${$numOfHours}&lang=en`;
+        settings.url = `https://dark-sky.p.rapidapi.com/44.4280,-110.5885?units=auto&extend=${$numOfHours}&lang=en`;
     } else if ($location === 'newYork'){
-        URL = `https://dark-sky.p.rapidapi.com/40.7128,-74.0060?units=auto&extend=${$numOfHours}&lang=en`;
+        settings.url = `https://dark-sky.p.rapidapi.com/40.7128,-74.0060?units=auto&extend=${$numOfHours}&lang=en`;
     } else if ($location === 'miami'){
-        URL = `https://dark-sky.p.rapidapi.com/25.7617,-80.1918?units=auto&extend=${$numOfHours}&lang=en`;
+        settings.url = `https://dark-sky.p.rapidapi.com/25.7617,-80.1918?units=auto&extend=${$numOfHours}&lang=en`;
     } else if ($location === 'zion'){
-        URL = `https://dark-sky.p.rapidapi.com/37.2982,-113.0263?units=auto&extend=${$numOfHours}&lang=en`;
+        settings.url = `https://dark-sky.p.rapidapi.com/37.2982,-113.0263?units=auto&extend=${$numOfHours}&lang=en`;
     }  else {
         console.log('location does not match listed options.')
     }
 }
 
-//Write function to change the URL variable when a different selection is made
+//On-Click function--- run the following-----
+
 $('button').on('click', function(){
+
+//Change the URL variable when a different selection is made
 $activities = $('select.typeOfT option:selected').val();
 $location = $('select.places option:selected').val();
 $startMonth = $('select.start_month option:selected').val();
@@ -52,5 +71,17 @@ $endDate = (`${$endMonth} ${$endDay}`);
 $numOfHours = (($endDay - $startDay)* 24);
 console.log($location, $activities , $startDate, $endDate , $numOfHours);
 runApi();
-console.log(URL);
+console.log(settings.url);
+
+//call the Ajax URL
+
+$.ajax(settings).done(function (response) {
+	console.log('Weather forcast is ready');
+    console.log(response);
+});
+
+// //Function to display new HTML Div after submit
+// $('#afterSubmit').
+
+
 });
